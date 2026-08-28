2d6490b6fbadfd22436edf6f2f029c8beb1a1c86
amasgari <amirabbas.asgari@ucalgary.ca>
2026-08-27 20:55:46 -0400

    Update talkmap/ contents by re-running /talkmap.py Set "talkmap_link" to true and edit Google Scholar link in _config.yml

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,7 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
-  ],
-  [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
-  ],
-  [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Automation of generating explicit formulas for the divisor class arithmetic of hyperelliptic curves<br />Alberta Number Theory Days XVI; Banff, AB, Canada",
+    51.175076,
+    -115.5720773
   ]
 ];
